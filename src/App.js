0cde4adebeb5864e0e5b9d40e790be08843462ad
Melvin <peepersm@gmail.com>
2020-02-03 14:44:42 -0500

    added NavBar and CSS

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React from "react";
-import "./App.css";
+import React, { useState, useEffect }from "react";
+import NavBar from "./components/navbar/NavBar.js";
 
 function App() {
+  const [NasaImages, setNavaImages] = useState([]);
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <div>
+        <NavBar />
+      </div>
     </div>
   );
 }
---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NavBar.css';
+
+function NavBar() {
+  return(
+    <div className="logo">
+      <h1 className="logo-text">NASA <span className="logo-text-second">Photo of the Day</span></h1>
+    </div>
+  )
+}
+
+export default NavBar;
